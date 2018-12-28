c5a0c582b8b9ad3a0fd15503abda87cf731778c6
lecocq <sgfdr>
2018-12-28 11:00:42 +0100

    feat: 8 Filtre

---
+++ src/Entity/PropertySearch.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lecocq
+ * Date: 27/12/2018
+ * Time: 16:17
+ */
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+
+
+
+class PropertySearch
+{
+    /**
+     * @var int|null
+     */
+    private $maxPrice;
+
+    /**
+     * @var int|null
+     * @Assert\Range(min="4",max="400")　
+     */
+    private $minSurface;
+
+    /**
+     * @return int|null
+     */
+    public function getMaxPrice():?int
+    {
+        return $this->maxPrice;
+    }
+
+    /**
+     * @param int|null $maxPrice
+     * @return PropertySearch
+     */
+    public function setMaxPrice(int $maxPrice):PropertySearch
+    {
+        $this->maxPrice = $maxPrice;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getMinSurface():?int
+    {
+        return $this->minSurface;
+    }
+
+    /**
+     * @param int|null $minSurface
+     * @return PropertySearch
+     */
+    public function setMinSurface(int $minSurface) :PropertySearch
+    {
+        $this->minSurface = $minSurface;
+        return $this;
+    }
+
+
+}
